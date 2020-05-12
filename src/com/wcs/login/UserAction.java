6f9458d3be9fc43a3dcf63fd9fdcc8743841276c
superJunF <30334636+superJunF@users.noreply.github.com>
2020-05-13 01:36:41 +0800

    Update UserAction.java

--- src/com/wcs/login/UserAction.java
+++ src/com/wcs/login/UserAction.java
@@ -24,7 +24,7 @@ public class UserAction extends ActionSupport{
 				getRequiredWebApplicationContext(ServletActionContext.getServletContext());
 		UserService userService = (UserService) applicationContext.getBean("userService");
 		String ss = "123456";
-		String dd = "2222BAD";
+		String dd = "2222BAD~~~";
 		String result = userService.login(userName,passWord);
 		
 		return result;
